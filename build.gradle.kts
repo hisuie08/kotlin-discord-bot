1d553a5b36a86d930827ef1987867ba3ca0388db
hisuie08 <keisyu0825@gmail.com>
2020-12-20 19:33:05 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    java
+    kotlin("jvm") version "1.4.21"
+    id("com.github.johnrengelman.shadow") version "6.1.0" //依存含めたjar作成のためのプラグイン
+}
+
+group = "com.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("net.dv8tion:JDA:4.2.0_211") //JDA Java/KotlinのDiscordAPIラッパー
+    testCompile("junit", "junit", "4.12")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
+
+tasks.shadowJar{
+    manifest{
+        attributes["Main-Class"] = "com.example.examplebot.MainKt"
+    }
+}
