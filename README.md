65dfab12dc9b00805ef8f3a405a6961691ab64ad
hisuie08 <keisyu0825@gmail.com>
2021-06-13 21:02:52 +0900

    readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # kotlin-discord-bot
- 
+
+Gradle/Kotlin JDAを使ったDiscord Botテンプレート
+
+## 注意
+このテンプレートには
+```bot.kt``` :イベントリスナー実装のBot
+```client.kt``` :Commandフレームワーク実装のBot
+の2種類があります。
+利用する方を
+```main.kt``` にリネームしてから起動してください。
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
     java
     kotlin("jvm") version "1.4.21"
-    id("com.github.johnrengelman.shadow") version "6.1.0" //依存含めたjar作成のためのプラグイン
+    application
+    id("com.github.johnrengelman.shadow") version "7.0.0" //依存含めたjar作成のためのプラグイン
 }
 
 group = "com.example"
@@ -9,24 +10,17 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://m2.dv8tion.net/releases")
     jcenter()
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("net.dv8tion:JDA:4.2.0_211") //JDA Java/KotlinのDiscordAPIラッパー
+    implementation("net.dv8tion:JDA:4.3.0_277")//JDA Java/KotlinのDiscordAPIラッパー
     implementation("com.jagrosh:jda-utilities:3.0.5") // JDAでコマンドを使うときのやつ
     testCompile("junit", "junit", "4.12")
 }
 
-tasks {
-    compileKotlin {
-        kotlinOptions.jvmTarget = "1.8"
-    }
-}
-
-tasks.shadowJar{
-    manifest{
-        attributes["Main-Class"] = "com.example.examplebot.MainKt"
-    }
+application{
+    mainClass.set("${group}.${rootProject.name}.MainKt")
 }
